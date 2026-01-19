a366a2c9790c85fb0d681e0718710d73ed857049
ReMatz90 <145059551+ReMatz90@users.noreply.github.com>
2026-01-19 09:09:13 +0100

    Add controlled invoice status transitions

--- Models/Invoice.cs
+++ Models/Invoice.cs
@@ -31,5 +31,60 @@ namespace DogBoarding.Models
         }
 
         #endregion
+
+        #region Status Transitions
+
+        public bool MarkAsSent()
+        {
+            if (Status != InvoiceStatus.Created)
+            {
+                return false;
+            }
+
+            Status = InvoiceStatus.Sent;
+            return true;
+        }
+
+        public bool MarkAsPaid()
+        {
+            if (Status != InvoiceStatus.Sent)
+            {
+                return false;
+            }
+
+            Status = InvoiceStatus.Paid;
+            return true;
+        }
+
+        public bool Cancel()
+        {
+            if (Status == InvoiceStatus.Paid)
+            {
+                return false;
+            }
+
+            Status = InvoiceStatus.Cancelled;
+            return true;
+        }
+
+        public void ForceStatus(InvoiceStatus newStatus)
+        {
+            if (!Enum.IsDefined(typeof(InvoiceStatus), newStatus))
+            {
+                throw new ArgumentException("Invalid invoice status.");
+            }
+
+            Status = newStatus;
+        }
+
+
+
+
+
+
+
+
+
+        #endregion
     }
 }
